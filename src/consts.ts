cf0d6dd4a722eec5b9d33279217e40e5b58e704b
DominixZ <domesterz@gmail.com>
2024-11-17 19:16:15 +0700

    change title

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'The DominixZ';
+export const SITE_TITLE = 'DominixZ';
 export const SITE_TAGLINE = 'Live Smart and Geeky'; // site tagline
 export const SITE_DESCRIPTION = 'Live Smart and Geeky'; // site meta description for SEO
